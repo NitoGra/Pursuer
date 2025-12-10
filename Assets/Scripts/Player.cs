a4cb6cdfe7025fa9a0ae926a011e5fd84c9ab98f
ArkNito <131981505+NitoGra@users.noreply.github.com>
2025-12-10 21:50:44 +0300

    Исправил Player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,65 +1,89 @@
 ﻿using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class Player : Movement
+namespace Scripts
 {
-    [SerializeField] private Camera _camera;
-    [Space]
-    [SerializeField] private float _moveSpeed = 4f;
-    [SerializeField] private float _jumpForce = 4f;
-    [SerializeField] private float _mouseSensitivity = 5;
-    [Space]
-    [SerializeField] private float _minVerticalAngle = -80f;
-    [SerializeField] private float _maxVerticalAngle = 80f;
-    private float _verticalLookRotation = 0f;
-
-    private void Start() => Cursor.lockState = CursorLockMode.Locked;
-
-    private void FixedUpdate()
+    internal class Player : MonoBehaviour
     {
-        HandleRotation();
-        HandleMovement();
-    }
+        private const float MoveCameraDelta = 0.1f;
+        private const float Heigth = 1.1f;
 
-    private void HandleRotation()
-    {
-        if (Mathf.Abs(Look.x) > 0.1f)
-            transform.Rotate(0f, Look.x * _mouseSensitivity * Time.fixedDeltaTime, 0f);
+        [SerializeField] private Camera _camera;
+        [SerializeField] private Rigidbody _rigidbody;
+        [Space]
+        [SerializeField] private float _moveSpeed = 4f;
+        [SerializeField] private float _jumpForce = 4f;
+        [SerializeField] private float _mouseSensitivity = 5;
+        [Space]
+        [SerializeField] private float _minVerticalAngle = -80f;
+        [SerializeField] private float _maxVerticalAngle = 80f;
 
-        if (Mathf.Abs(Look.y) > 0.1f && _camera != null)
+        private float _verticalLookRotation = 0f;
+        private InputSystem _inputSystem;
+
+
+        private void Awake()
         {
-            _verticalLookRotation -= Look.y * _mouseSensitivity * Time.fixedDeltaTime;
-            _verticalLookRotation = Mathf.Clamp(_verticalLookRotation, _minVerticalAngle, _maxVerticalAngle);
-            _camera.transform.localRotation = Quaternion.Euler(_verticalLookRotation, 0f, 0f);
-        }
-    }
-
-    private void HandleMovement()
-    {
-        if (MoveInput == Vector2.zero)
-        {
-            Rb.linearVelocity = new Vector3(0f, Rb.linearVelocity.y, 0f);
-            return;
+            Cursor.lockState = CursorLockMode.Locked;
+            _inputSystem = new(Jump);
         }
 
-        Vector3 cameraForward = _camera.transform.forward;
-        Vector3 cameraRight = _camera.transform.right;
+        private void OnEnable()
+        {
+            _inputSystem.Enable();
+        }
 
-        cameraForward.y = 0f;
-        cameraRight.y = 0f;
+        private void FixedUpdate()
+        {
+            HandleRotation();
+            HandleMovement();
+        }
 
-        cameraForward.Normalize();
-        cameraRight.Normalize();
+        private void OnDisable()
+        {
+            _inputSystem.Disable();
+        }
 
-        Vector3 movement = (cameraForward * MoveInput.y + cameraRight * MoveInput.x) * _moveSpeed;
-        Rb.linearVelocity = new Vector3(movement.x, Rb.linearVelocity.y, movement.z);
-    }
+        private void HandleRotation()
+        {
+            if (Mathf.Abs(_inputSystem.Look.x) > MoveCameraDelta)
+                transform.Rotate(0f, _inputSystem.Look.x * _mouseSensitivity * Time.fixedDeltaTime, 0f);
 
-    protected override void Jump(InputAction.CallbackContext context)
-    {
-        if (Physics.Raycast(transform.position, Vector3.down, 1.1f) == false)
-            return;
+            if (Mathf.Abs(_inputSystem.Look.y) > MoveCameraDelta && _camera != null)
+            {
+                _verticalLookRotation -= _inputSystem.Look.y * _mouseSensitivity * Time.fixedDeltaTime;
+                _verticalLookRotation = Mathf.Clamp(_verticalLookRotation, _minVerticalAngle, _maxVerticalAngle);
+                _camera.transform.localRotation = Quaternion.Euler(_verticalLookRotation, 0f, 0f);
+            }
+        }
 
-        Rb.linearVelocity += _jumpForce * Vector3.up;
+        private void HandleMovement()
+        {
+            if (_inputSystem.MoveInput == Vector2.zero)
+            {
+                _rigidbody.linearVelocity = new Vector3(0f, _rigidbody.linearVelocity.y, 0f);
+                return;
+            }
+
+            Vector3 cameraForward = _camera.transform.forward;
+            Vector3 cameraRight = _camera.transform.right;
+
+            cameraForward.y = 0f;
+            cameraRight.y = 0f;
+
+            cameraForward.Normalize();
+            cameraRight.Normalize();
+
+            Vector3 movement = (cameraForward * _inputSystem.MoveInput.y + cameraRight * _inputSystem.MoveInput.x) * _moveSpeed;
+            _rigidbody.linearVelocity = new Vector3(movement.x, _rigidbody.linearVelocity.y, movement.z);
+        }
+
+        private void Jump(InputAction.CallbackContext context)
+        {
+            if (Physics.Raycast(transform.position, Vector3.down, Heigth) == false)
+                return;
+
+            _rigidbody.linearVelocity += _jumpForce * Vector3.up;
+        }
     }
 }
